04ef0453b684d0d53d844053666ce7c996b0d57a
Caio Fonseca <caiofonseca1990@gmail.com>
2023-07-17 10:29:58 -0300

    mobile

--- src/components/MenuHamburger.jsx
+++ src/components/MenuHamburger.jsx
@@ -22,14 +22,14 @@ const MenuHamburger = () => {
       <div id="menuhamb" className="bg-black">
         {openNavBar ? (
           // icon close menu
-          <div className=" flex flex-col h-[100%] w-screen modal items-end ">
+          <div className=" flex flex-col h-[100%] w-[100%] modal items-end ">
             <AiOutlineClose
               onClick={openNav}
               className="text-[2rem] hover:scale-125 hover:text-purple-400 
               text-neutral-200 cursor-pointer top-5 fixed right-5 bg-transparent ease-out z-50"
             />  
-            <nav className="container text-white w-[22rem] h-screen max-[510px]:w-screen max-[510px]:h-[40%] text-center duration-200 pt-28 shadow-md border">
-              <ul className="flex flex-col justify-center gap-8 relative pb-10 ">
+            <nav className="container text-white w-[22rem] h-screen max-[510px]:w-screen max-[510px]:h-[40%] text-center duration-200 pt-28 shadow-md ">
+              <ul className="flex flex-col justify-center gap-8 relative pb-10">
                 {Menu.map((menu,index) => (
                   <a href={menu.link} key={index}
                   className="hover:text-purple-400 font-semibold hover:scale-110 bg-transparent duration-200 ">
